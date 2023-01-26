2510f27d3c0601055281379a9b9c1c76066d1bbe
AlinaBerns <burns.alina@yandex.ru>
2023-01-26 15:45:55 +0100

    opdracht_4

--- src/be/intecbrussel/MainApp.java
+++ src/be/intecbrussel/MainApp.java
@@ -1,2 +1,19 @@
-package be.intecbrussel;public class MainApp {
+package be.intecbrussel;
+
+public class MainApp {
+    public static void main(String[] args) {
+        int t = -1;
+
+        for (int i = 9; i >= 0; i--) {
+            t++;
+
+            System.out.print(i + " - " + t + " - ");
+
+            }
+
+        }
 }
+
+
+
+
